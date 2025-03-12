8ec1dcc8c18249c9fc5e6a400f543bde9a6299a6
Jeremy Brinkworth <88738953+jbrinkw@users.noreply.github.com>
2025-03-12 18:12:53 -0400

    fixed missing username in git clone link

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -9,7 +9,7 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 
 # Clone the repository
-git clone https://github.com/your-username/luna-personal-assistant.git
+git clone https://github.com/jbrinkw/luna-personal-assistant.git
 cd luna-personal-assistant
 
 # Build and run Docker container
