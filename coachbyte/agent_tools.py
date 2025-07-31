9b5aca651bcb140f2761fd133105a9f11bd0ab98
Jeremy Brinkworth <88738953+jbrinkw@users.noreply.github.com>
2025-07-31 05:09:00 -0400

    moved .gitignore

--- coachbyte/agent_tools.py
+++ coachbyte/agent_tools.py
@@ -1,20 +1,33 @@
 """OpenAI Agent FunctionTool wrappers for CoachByte tools."""
 
 from agents import function_tool
-import coachbyte.tools as base
+from tools import *
 
 # Wrap each plain function
-new_daily_plan = function_tool(strict_mode=False)(base.new_daily_plan)
-get_today_plan = function_tool(strict_mode=False)(base.get_today_plan)
-log_completed_set = function_tool(strict_mode=False)(base.log_completed_set)
-complete_planned_set = function_tool(strict_mode=False)(base.complete_planned_set)
-update_summary = function_tool(strict_mode=False)(base.update_summary)
-get_recent_history = function_tool(strict_mode=False)(base.get_recent_history)
-set_weekly_split_day = function_tool(strict_mode=False)(base.set_weekly_split_day)
-get_weekly_split = function_tool(strict_mode=False)(base.get_weekly_split)
-run_sql = function_tool(strict_mode=False)(base.run_sql)
-arbitrary_update = function_tool(strict_mode=False)(base.arbitrary_update)
-set_timer = function_tool(strict_mode=False)(base.set_timer)
-get_timer = function_tool(strict_mode=False)(base.get_timer)
+new_daily_plan = function_tool(strict_mode=False)(new_daily_plan)
+get_today_plan = function_tool(strict_mode=False)(get_today_plan)
+log_completed_set = function_tool(strict_mode=False)(log_completed_set)
+complete_planned_set = function_tool(strict_mode=False)(complete_planned_set)
+update_summary = function_tool(strict_mode=False)(update_summary)
+get_recent_history = function_tool(strict_mode=False)(get_recent_history)
+set_weekly_split_day = function_tool(strict_mode=False)(set_weekly_split_day)
+get_weekly_split = function_tool(strict_mode=False)(get_weekly_split)
+run_sql = function_tool(strict_mode=False)(run_sql)
+arbitrary_update = function_tool(strict_mode=False)(arbitrary_update)
+set_timer = function_tool(strict_mode=False)(set_timer)
+get_timer = function_tool(strict_mode=False)(get_timer)
 
-__all__ = base.__all__
+__all__ = [
+    "new_daily_plan",
+    "get_today_plan", 
+    "log_completed_set",
+    "complete_planned_set",
+    "update_summary",
+    "get_recent_history",
+    "set_weekly_split_day",
+    "get_weekly_split",
+    "run_sql",
+    "arbitrary_update",
+    "set_timer",
+    "get_timer",
+]
